6a763113a99affb9f42b56d2f48aafdfd188954d
Apoffis <abc22_08@mail.ru>
2022-11-03 16:38:19 +0400

    module4 final Task commit 3

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,16 +1,26 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
+
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
         List<String> devs = new ArrayList<>();
         for(Map.Entry<String, Set<String>> proj : projects.entrySet()) {
             if (proj.getValue().contains(developer)) devs.add(proj.getKey());
         }
+        Collections.sort(devs, new LengthFirstComparator());
+        Collections.reverse(devs);
         return devs;
+    }
+
+    public class LengthFirstComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            if (o1.length()!=o2.length()) {
+                return o1.length()-o2.length();
+            }
+            return o1.compareTo(o2);
+        }
     }
 }
